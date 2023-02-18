6218267f66ef52b9d5252fa635fca583adf1f856
Zoolian <ilovemylyf247@gmail.com>
2023-02-18 00:15:27 +0000

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("Positive");
+	}
+	else if (n < 0)
+	{
+		printf("Negetive");
+	}
+	else
+	{
+		print("zero");
+	}
+	return (0);
+}
