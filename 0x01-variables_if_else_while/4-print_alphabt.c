8239a10115201101a84947ee88870b3583372b1b
Zoolian <ilovemylyf247@gmail.com>
2023-02-20 11:43:35 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints alphabets in lowercase then uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lwalp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i != 4 && i != 17)
+		{
+		putchar(lwalp[i]);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
