7ff6c45040c86384cc904ba0683438aefe5c42c9
Zoolian <ilovemylyf247@gmail.com>
2023-02-24 17:22:01 +0000

    Together can

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,14 @@
 void print_triangle(int size)
 {
 	int i, j;
-
-	for (i = 1; i <= size && size > 0; i++)
+i
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 1; j < size - i; j++)
 		{
 			_putchar(' ');
 		}
-		for (j = 0; j < i; j++)
+		for (j = 1; j < i; j++)
 		{
 			_putchar('#');
 		}
