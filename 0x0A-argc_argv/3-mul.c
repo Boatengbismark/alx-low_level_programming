d89691b6a5273da6bd82dfa00f96ecfbfb06318f
Zoolian <ilovemylyf247@gmail.com>
2023-03-10 19:09:28 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 1;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	for (i = 1; i < argc; i++)
+	{
+		sum *= atoi(argv[i]);
+	
+	}
+
+	printf("%d\n",sum);
+	return (0);
+}
