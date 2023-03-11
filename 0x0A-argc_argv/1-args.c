83f64e493ed738e1351544dce4ccb5d7af0e1fef
Zoolian <ilovemylyf247@gmail.com>
2023-03-11 01:33:22 +0000

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argv[i] == " ")
+		{
+			sum += 0;
+		}
+		else
+		sum ++;
+	}
+
+	printf("%d\n", sum - 1);
 }
