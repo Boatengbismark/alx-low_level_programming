c7152b7b783f9074bd598bf29b334e0b9e5592e8
Zoolian <ilovemylyf247@gmail.com>
2023-03-15 17:13:44 +0000

    fix_error' ' '

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 == NULL)
 		return ("");
 
-	while (*s1 != '\0')
+	while (s1[len1] != '\0')
 		len1++;
-	while (*s2 != '\0')
+	while (s2[len2] != '\0')
 		len2++;
        	length = len1 + len2 + 1;
 
